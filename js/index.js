a17ae880cf2ef175bd646f360c03da5b791b7d48
Agustinusmulianto <agustinus.yan96@gmail.com>
2025-06-15 11:17:01 +0700

    menambahkan index.hmtl, index.js dan syle.css

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const tittle = document.querySelector('title');
+tittle.textContent = 'Kaisar Store';
+const app = document.getElementById('app');
+
+app.innerHTML = `
+  <h1>Welcome to My Web App</h1>
+  `;
